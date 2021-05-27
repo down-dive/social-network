f40121e87a45203f98f29c5777c3d70d32c2047b
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-27 11:36:20 -0700

    thought model

--- models/User.js
+++ models/User.js
@@ -2,31 +2,31 @@ const { Schema, model } = require('mongoose');
 
 const UserNameSchema = new Schema({
     username: {
-      type: String,
-      unique : true, 
-      required : true,
-      trim: true
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
     },
-   email: {
-      type: String,
-      unique : true, 
-      validate: {
-        validator: function(v) {
-            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+    email: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
         },
-        message: "Please enter a valid email"
-    },
-    required: [true, "Email required"]
+        required: [true, "Email required"]
     },
     thoughts: {
-    // _id reference for thought model
+        // _id reference for thought model
     },
     friends: {
         // _id values referencing the User model (self-reference)
 
     }
-  });
+});
 
-  const User = model('User', UserNameSchema);
+const User = model('User', UserNameSchema);
 
 module.exports = User;
