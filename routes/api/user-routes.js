1292ff95d64946e6c2450f97d6425881059351f8
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-28 11:00:43 -0700

    routes for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
-// Set up GET all and POST at /api/pizzas
+// Set up GET all and POST at /api/users
 router
   .route('/')
   .get()
   .post();
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+// Set up GET one, PUT, and DELETE at /api/users/:id
 router
   .route('/:id')
   .get()
