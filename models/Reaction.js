529bed97b3ab8a2a91d85f053f2a736cd38e5df7
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-28 10:39:23 -0700

    controller for user

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,30 +2,31 @@ const { ObjectId } = require('bson');
 const { Schema, model } = require('mongoose');
 
 const ReactionsSchema = new Schema({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-      },
-    reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280      },
-    username: {
-        type: String,
-        required: true,   
-       },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-          get: createdAtVal => dateFormat(createdAtVal)
-      },
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
 },
-{
-  toJSON: {
+  {
+    toJSON: {
       getters: true,
-      },
-}
-  );
+    },
+  }
+);
 
 const Reaction = model('Reaction', ReactionsSchema);
 
