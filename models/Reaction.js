2d754ca82d091ba85541d137f0f5bb00b4fdbf4e
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-27 22:33:04 -0700

    changing email

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { ObjectId } = require('bson');
+const { Schema, model } = require('mongoose');
+
+const ReactionsSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280      },
+    username: {
+        type: String,
+        required: true,    },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: createdAtVal => dateFormat(createdAtVal)
+      },
+}
+  );
+
+const Reaction = model('Reaction', ReactionsSchema);
+
+module.exports = Reaction;
